7df5959bba16e3f597069a8d1a080b544e19f68c
xlp67 <thiiagowilliam@gmail.com>
2025-12-19 18:16:23 -0300

    testando workflow

--- terraform/modules/gcp/dataflow/main.tf
+++ terraform/modules/gcp/dataflow/main.tf
@@ -3,7 +3,7 @@
 resource "google_dataflow_flex_template_job" "job" {
   provider                      = google-beta
   region                        = var.region
-  name                    = "job_${var.image_tag}"
+  name                    = "job-${var.image_tag}"
   container_spec_gcs_path       = var.dataflow_container_spec_gcs_path
   skip_wait_on_job_termination = true
   max_workers                   = 1
